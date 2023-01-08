dcead949c831de0deadcd72d59feec9a0b91ba1d
joey <jseris44@vt.edu>
2023-01-07 20:38:05 -0500

    Adding engine to use to_sql() function. Hiding additional lines for use in future versions

--- MySQL_FillTables.py
+++ MySQL_FillTables.py
@@ -1,15 +1,18 @@
-import pandas
+import pandas as pd
+import sqlalchemy
 import mysql.connector
 from os import listdir
 from os.path import isfile, join
 from MellonAccess import MellonArray
 
 dataloc = "C:\\Users\\joeye\\Desktop\\Projets\\Programming\\Data\\F1Data_archive\\" #Directory with all F1 data files
-f1files = [f for f in listdir(dataloc) if isfile(join(dataloc, f))]  #runs through files in F1 data directory and joins names as strings to list
+f1files = [f for f in listdir(dataloc) if isfile(join(dataloc, f))]  #Gathering data files names
+
 
 mc = MellonArray() #Pulls username and password to access MySQL
-mcu = str(mc[0])[2:-2] #Changes the username list object to a string and removes [""]
-mcp = str(mc[1])[2:-2] #Changes the password list object to a string and removes [""]
+mcu = str(mc[0])[2:-2] #Cleaning string username for SQL connector
+mcp = str(mc[1])[2:-2] #Cleaning string password for SQL connector
+engine = sqlalchemy.create_engine("mysql://" + mcu + ":" + mcp + "@localhost/F1") #to_sql function requires a sqlalchemy engine. Unable to use mysql connector.
 
 cnt = mysql.connector.connect(
     user= mcu,
