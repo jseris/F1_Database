6293a4dc4868051bb9bac8ea0f3c8e0e576110a4
joey <jseris44@vt.edu>
2023-03-03 21:21:36 -0500

    fixes class object to be imported in other modules for use

--- MellonAccess.py
+++ MellonAccess.py
@@ -16,19 +16,18 @@ def MellonArray():
 
 class Creds:
     mc = MellonArray() #Pulls username and password to access MySQL
-    mcu = str(mc[0])[2:-2] #Changes the username list object to a string and removes [""]
-    mcp = str(mc[1])[2:-2] #Changes the password list object to a string and removes [""]
-    mch = str(mc[2])[2:-2] #Changes the host list object to a string and removes [""]
+    def __init__(mcu,mcp,mch)
+        mcu = str(mc[0])[2:-2] #Changes the username list object to a string and removes [""]
+        mcp = str(mc[1])[2:-2] #Changes the password list object to a string and removes [""]
+        mch = str(mc[2])[2:-2] #Changes the host list object to a string and removes [""]
 
-def crt_crsr():
-    cntpass = Creds()
-    cnt = mysql.connector.connect(
-        user= cntpass.mcu,
-        password= cntpass.mcp,
-        host= cntpass.mch
-        )
+    def crt_crsr():
+        cntpass = Creds()
+        cnt = mysql.connector.connect(
+            user= cntpass.mcu,
+            password= cntpass.mcp,
+            host= cntpass.mch
+            )
 
-    mycursor = cnt.cursor()
-    return mycursor
-
-crt_crsr()
+        mycursor = cnt.cursor()
+        return mycursor
