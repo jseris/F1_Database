1ae4a3442a2012da5fe632b4cf9edf24e3ebbe5a
joey <jseris44@vt.edu>
2023-03-03 19:16:13 -0500

    New bash code to execute python files

---
+++ LoadDB.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+#!/usr/bin/env bash python3 Create_F1_DB.py
+#!/usr/bin/env bash python3 Create_Tables.py
+#!/usr/bin/env bash python3 MySQL_FillTables.py
+#!/usr/bin/env bash python3 MySQL_Queries.py
