4310066f1e6518ea7a8e8ccdbab51a47c1627567
joey <jseris44@vt.edu>
2023-03-03 21:05:06 -0500

    Added class object to streamline log-in process

--- MellonAccess.py
+++ MellonAccess.py
@@ -1,5 +1,6 @@
 import csv
 import os.path
+import mysql.connector
 
 def MellonArray():
     file_name = "MySQLMellon.txt"
@@ -11,7 +12,23 @@ def MellonArray():
 
         for row in reader:
             acarray.append(row) #inserts username and password, respectively, in list
-
     return acarray
 
-MellonArray()
+class Creds:
+    mc = MellonArray() #Pulls username and password to access MySQL
+    mcu = str(mc[0])[2:-2] #Changes the username list object to a string and removes [""]
+    mcp = str(mc[1])[2:-2] #Changes the password list object to a string and removes [""]
+    mch = str(mc[2])[2:-2] #Changes the host list object to a string and removes [""]
+
+def crt_crsr():
+    cntpass = Creds()
+    cnt = mysql.connector.connect(
+        user= cntpass.mcu,
+        password= cntpass.mcp,
+        host= cntpass.mch
+        )
+
+    mycursor = cnt.cursor()
+    return mycursor
+
+crt_crsr()
